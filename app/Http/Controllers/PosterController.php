a7384fb4bde9389c5fe838c1ad190374fe4d094c
GONGXI\Administrator <heavengongxi@gmail.com>
2017-03-04 21:55:14 +0800

    test post

--- app/Http/Controllers/PosterController.php
+++ app/Http/Controllers/PosterController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Router;
 
 class PosterController extends Controller
 {
     public function index(Request $request)
     {
+    	$router = new Router;
+    	$router->content = $request->get('content');
+    	$router->save();
     	return $request->get('content');
     }
 }
